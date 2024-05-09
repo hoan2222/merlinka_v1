d19fefb00f4993a16f368150c24b3d799a938e8c
anelnoraa <noah.troebs@gmail.com>
2024-05-09 17:41:15 +0200

    done

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,12 +1,17 @@
-import { getDocuments } from 'outstatic/server'
+import { getDocuments } from 'outstatic/server';
+
+import Preis from '../components/ui/preis/preis';
 
 
 
 export default async function PreisListe() {
     const posts = await getData();
     return posts.map((post) => {
+        console.log(post.content)
+        const content = post.content.trim().split("\n- ");
+        console.log(content);
         return (
-            <h1>{post.content}</h1>
+            <Preis content={content} name={post.title} preis={post.preis} image={post.coverImage} />
         )
 
     }
@@ -15,6 +20,7 @@ export default async function PreisListe() {
 
 async function getData() {
     const posts = getDocuments('Pakete', ['title', 'preis', 'content', 'coverImage'])
+
 
     return posts
 }
