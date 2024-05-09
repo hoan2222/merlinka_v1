d2b1c85c59a97605df18951c4c932fcf8681c11c
anelnoraa <noah.troebs@gmail.com>
2024-05-09 17:21:43 +0200

    done

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import About from "./components/about";
 import Contact from "./components/contact";
 import Preisliste from "./components/preisliste";
 import Impressum from "./components/impressum";
-import PreisListe from "./components/preisliste/test.jsx"
 
 export default function Home() {
 
@@ -12,7 +11,7 @@ export default function Home() {
       <Head />
       <div className="flex flex-col py-48 justify-start w-full">
         <About />
-        <PreisListe />
+
         <Preisliste />
         <Contact />
 
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,17 +1,24 @@
 import { getDocuments } from 'outstatic/server'
 
+import Preis from '../components/ui/preis/preis';
 
 
 export default async function PreisListe() {
     const posts = await getData();
-    return posts.map((post) => <h1>{post.content}</h1>)
+    return posts.map((post) => {
+
+        <h1>{post.content}</h1>
+    }
+    )
 }
 
 async function getData() {
-    const posts = getDocuments('Pakete', ['title', 'content'])
+    const posts = getDocuments('Pakete', ['title', 'preis', 'content', 'coverImage'])
 
     return posts
 }
+
+
 
 
 
