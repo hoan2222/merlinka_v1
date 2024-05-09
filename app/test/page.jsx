b7f75917ad705cdb1807508fc124aff84f733dd5
anelnoraa <noah.troebs@gmail.com>
2024-05-09 17:09:12 +0200

    done

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -4,11 +4,14 @@ import { getDocuments } from 'outstatic/server'
 
 export default async function PreisListe() {
     const posts = await getData();
-    return posts.map((post) => <h1>{post.title}</h1>)
+    return posts.map((post) => <h1>{post.content}</h1>)
 }
 
 async function getData() {
-    const posts = getDocuments('pakete')
+    const posts = getDocuments('Pakete', ['title', 'content'])
 
     return posts
 }
+
+
+
