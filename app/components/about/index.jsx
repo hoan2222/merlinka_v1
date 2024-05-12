0187267723fab7c3db0dbfb5e35f2c77fd70fd4d
anelnoraa <noah.troebs@gmail.com>
2024-05-13 00:39:39 +0200

    done

--- app/components/about/index.jsx
+++ app/components/about/index.jsx
@@ -1,15 +1,30 @@
+'use client'
 
-
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function About() {
+
+    const [open, setOpen] = useState(false);
+
+    const buttonContent = [
+        "mehr...",
+        "weniger"
+    ]
 
     return (
         <div className="flex flex-col sm:flex-row justify-start space-y-6 px-12 sm:space-y-0 sm:space-x-20 sm:pt-48 pt-36" id="about">
             <h1 className="sm:hidden text-5xl font-semibold">About me</h1>
             <img src="/about.jpg" className="w-[514px] object-scale-down" />
-            <div className="flex flex-col space-y-8 sm:w-[40%]">
+            <motion.div
+                className="flex flex-col space-y-4 sm:space-y-8 sm:w-[40%]"
+                layout
+            >
                 <h1 className="hidden sm:block text-6xl font-semibold">About me</h1>
-                <p className="text-lg">Hallo ihr Lieben! Ich bin Merle. <br /><br />
+                <motion.p
+                    className={`text-lg ${open ? 'h-fit' : 'h-[20vh]'} sm:h-fit overflow-hidden`}
+                    layout
+                >Hallo ihr Lieben! Ich bin Merle. <br /><br />
                     Ich stehe so gern hinter der Kamera und fange die besonderen, kleinen und feinen Momente des
                     Lebens ein.<br />
                     Ich liebe das Licht, wenn es Morgens die ersten Staubkörner des Tages sichtbar tanzen lässt. Ich
@@ -20,9 +35,22 @@ export default function About() {
                     Das wär’s!<br />
                     Ich freue mich sehr auf euch und kann es kaum erwarten, eure Zeitmomente festzuhalten, für
                     immer.
-                </p>
-            </div>
+                </motion.p>
+                <p className="sm:hidden" onClick={() => {
+                    if (open) {
+                        const element = document.getElementById("about");
+                        element?.scrollIntoView({
+                            behavior: "smooth",
+                        })
+                        setOpen(!open)
+                    } else {
+                        setOpen(!open);
+                    }
+
+                }}>{open ? buttonContent[1] : buttonContent[0]}</p>
+            </motion.div>
         </div>
 
     )
-}   
+}
+
