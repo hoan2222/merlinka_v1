e911a6ba5e7ee4d735ea11560b6be4264c6c0692
anelnoraa <noah.troebs@gmail.com>
2024-05-13 11:15:17 +0200

    changed route

--- app/api/outstatic/[[...ost]]/route.js
+++ app/api/outstatic/[[...ost]]/route.js
@@ -1,12 +1,12 @@
 import { OutstaticApi } from 'outstatic'
 import { getDocumentSlugs } from 'outstatic/server'
 
+export async function generateStaticParams() {
+    const posts = getDocumentSlugs('posts')
+    return posts.map((slug) => ({ slug }))
+}
 
 export const GET = OutstaticApi.GET
 
 export const POST = OutstaticApi.POST
 
-export async function generateStaticParams() {
-    const posts = getDocumentSlugs('posts')
-    return posts.map((slug) => ({ slug }))
-}
