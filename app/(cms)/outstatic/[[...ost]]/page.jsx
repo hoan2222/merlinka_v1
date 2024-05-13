07c6a35c625bd0d2f1af964d40fb2b6cf69595e0
anelnoraa <noah.troebs@gmail.com>
2024-05-13 10:44:47 +0200

    generateStaticParams added

--- app/(cms)/outstatic/[[...ost]]/page.jsx
+++ app/(cms)/outstatic/[[...ost]]/page.jsx
@@ -2,6 +2,11 @@ import 'outstatic/outstatic.css'
 import { Outstatic } from 'outstatic'
 import { OstClient } from 'outstatic/client'
 
+export async function generateStaticParams() {
+    const posts = getDocumentSlugs('posts')
+    return posts.map((slug) => ({ slug }))
+}
+
 export default async function Page({ params }) {
     const ostData = await Outstatic()
     return <OstClient ostData={ostData} params={params} />
