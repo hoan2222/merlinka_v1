f440cac94641f0a8b0289cb6fa5b960246c2dcb1
anelnoraa <noah.troebs@gmail.com>
2024-05-13 11:12:26 +0200

    test

--- app/api/outstatic/[[...ost]]/page.jsx
+++
@@ -1,6 +0,0 @@
-import { getDocumentSlugs } from 'outstatic/server'
-
-export async function generateStaticParams() {
-    const posts = getDocumentSlugs('posts')
-    return posts.map((slug) => ({ slug }))
-}
--- app/api/outstatic/[[...ost]]/route.js
+++ app/api/outstatic/[[...ost]]/route.js
@@ -1,6 +1,7 @@
 import { OutstaticApi } from 'outstatic'
 import { getDocumentSlugs } from 'outstatic/server'
 
+
 export const GET = OutstaticApi.GET
 
 export const POST = OutstaticApi.POST
