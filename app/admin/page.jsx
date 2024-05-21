493322d6a2dce77ad3ebd01423df6838aa82119e
anelnoraa <noah.troebs@gmail.com>
2024-05-21 22:28:06 +0200

    done changes on admin page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,16 +1,19 @@
+'use client'
 
+import { useRouter } from "next/navigation"
 
 
 export default function Admin() {
+
+    const router = useRouter()
+
     return (
         <div className=" flex justify-center pt-20 ">
             <div className="flex flex-col space-y-20">
                 <h1 className=" text-5xl font-bold">Merle bist du&apos;s ?</h1>
-                <a href="/merlinka_v1">
-                    <div className=" flex justify-center rounded-full border border-black py-8">
-                        <p className=" hover:font-semibold">Ne, leider nicht...</p>
-                    </div>
-                </a>
+                <div className=" flex justify-center rounded-full border border-black py-8" onClick={() => router.back()}>
+                    <p className=" hover:font-semibold">Ne, leider nicht...</p>
+                </div>
                 <a href="https://app.contentful.com/spaces/72vr5remawwr/home">
                     <div className=" flex justify-center rounded-full border border-black py-8">
                         <p className=" hover:font-semibold">Jap here I am</p>
