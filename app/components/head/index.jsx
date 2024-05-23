ecb75cce56f16caaa2d5f1972566799ee864b383
anelnoraa <noah.troebs@gmail.com>
2024-05-23 10:00:13 +0200

    done

--- app/components/head/index.jsx
+++ app/components/head/index.jsx
@@ -5,7 +5,7 @@ export default function Head() {
     return (
 
         <div className=" flex justify-center h-36 sm:h-44" id="head">
-            <img src="/merlinka_v1/Merlinka_LOGO_v1.2.svg" alt="Merlinja Fotografie" />
+            <img src="/merlinka_v1/Merlinka_LOGO_v1.2.svg" alt="Merlinka Fotografie" />
         </div>
     )
 } 
